44458acea4838539d70edabb66c12a09fa69d5ba
jeffcheng1234 <36403947+jeffcheng1234@users.noreply.github.com>
2019-10-27 02:40:06 -0700

    Button feature to filter init loader
    need to switch the button toggle state in UI

--- app/src/main/java/com/example/tal/ServeAsyncTaskLoader.java
+++ app/src/main/java/com/example/tal/ServeAsyncTaskLoader.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import android.content.AsyncTaskLoader;
+import android.widget.ToggleButton;
 
 import static com.example.tal.MapsActivity.LOG_TAG;
 
@@ -17,10 +18,12 @@ public class ServeAsyncTaskLoader extends AsyncTaskLoader< HashMap< String,List<
     private String bikeShareLink = "https://bikeshare.metro.net/stations/json";
     private String mApiKey;
     private String mLogTag;
+    private HashMap buttons;
     private HashMap dataRepository = new HashMap();
 
-    public ServeAsyncTaskLoader(Context context) {
+    public ServeAsyncTaskLoader(Context context, HashMap buttons) {
         super(context);
+        this.buttons = buttons;
 //        mQuery = query;
 //        mBaseLink = base_link;
 //        mApiKey = api_key;
@@ -42,22 +45,26 @@ public class ServeAsyncTaskLoader extends AsyncTaskLoader< HashMap< String,List<
 //        for (int i = 0; i < output.size(); i++) {
 //            Log.v("bird",output.get(i).loc+"");
 //        }
-        //creating ford objects
-        String jsonFordResponse = Utils.makeHttpRequest(mQuery,bikeShareLink,mApiKey,mLogTag);
-        Ford ford = new Ford(Service.start,Service.start,Service.start,Service.end);
-        Log.i("json",jsonFordResponse);
-        dataRepository.put(ford.name,ford.extractServices(jsonFordResponse));
-        //creating bird objects
-        String jsonBirdResponse = Utils.makeHttpRequest(mQuery,birdLink,mApiKey,mLogTag);
-        Bird bird = new Bird(Service.start,Service.start,Service.end);
-        Log.i("json",jsonBirdResponse);
-        dataRepository.put(bird.name,bird.extractServices(jsonBirdResponse));
-        //create ford_park objects
-        String jsonFord_parkResponse = Utils.makeHttpRequest(mQuery,bikeShareLink,mApiKey,mLogTag);
-        Ford_park ford_park= new Ford_park(Service.end);
-        Log.i("json",jsonFord_parkResponse);
-        dataRepository.put(Ford_park.name,ford_park.extractParking(jsonFord_parkResponse));
+        if((boolean)buttons.get("Bird")){ //When Bird button is initially pressed
+            //creating bird objects
+            String jsonBirdResponse = Utils.makeHttpRequest(mQuery,birdLink,mApiKey,mLogTag);
+            Bird bird = new Bird(Service.start,Service.start,Service.end);
+            Log.i("json",jsonBirdResponse);
+            dataRepository.put(bird.name,bird.extractServices(jsonBirdResponse));
+        }
+        if((boolean)buttons.get("Go Bike")){ //When Go Bike button is initially pressed
+            //creating ford objects
+            String jsonFordResponse = Utils.makeHttpRequest(mQuery,bikeShareLink,mApiKey,mLogTag);
+            Ford ford = new Ford(Service.start,Service.start,Service.start,Service.end);
+            Log.i("json",jsonFordResponse);
+            dataRepository.put(ford.name,ford.extractServices(jsonFordResponse));
 
+            //create ford_park objects
+            String jsonFord_parkResponse = Utils.makeHttpRequest(mQuery,bikeShareLink,mApiKey,mLogTag);
+            Ford_park ford_park= new Ford_park(Service.end);
+            Log.i("json",jsonFord_parkResponse);
+            dataRepository.put(Ford_park.name,ford_park.extractParking(jsonFord_parkResponse));
+        }
         return dataRepository;
     }
 }
