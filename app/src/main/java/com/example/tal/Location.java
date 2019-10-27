889310cb8a51316c2d39a15c35642b2c66932dd4
yanlarry <57030616+yanlarry@users.noreply.github.com>
2019-10-26 17:20:46 -0700

    Updated Retrieval again

--- app/src/main/java/com/example/tal/Location.java
+++ app/src/main/java/com/example/tal/Location.java
@@ -7,4 +7,13 @@ public class Location {
         this.x = x;
         this.y = y;
     }
+
+    //returns a double representing the approximate absolute displacement between two locations in miles
+    public static double displacement_between(Location one, Location two) {
+        return Math.sqrt(Math.pow((one.x-two.x)*69.172, 2) + Math.pow(((Math.cos(one.x*Math.PI/180)*one.y-(Math.cos(two.x*Math.PI/180)*two.y)))*69.172, 2));
+        //uh I hope that was right. Then again we're using small distances so... why don't we just use a planar version...
+
+        /*return 2*asin(sqrt((sin((one.x-two.x)/2))^2 +
+                cos(one.x)*cos(two.x)*pow((sin((one.y-two.y)/2), 2)));*/
+    }
 }
--- app/src/main/java/com/example/tal/Retrieval.java
+++ app/src/main/java/com/example/tal/Retrieval.java
@@ -23,6 +23,8 @@ public class Retrieval {
         //return arraylist of stufff;
     }
 
+    public static double radius = 10; // in miles, I think
+
     public static ArrayList<Service> get_ford(){
         ArrayList<Service> ford_list = new ArrayList<Service>();
         try {
@@ -32,8 +34,10 @@ public class Retrieval {
                 JSONObject station = jsonBirds.getJSONObject(i);
                 if (station.getInt("availableBikes") > 0 && station.getString("statusValue").equals("In Service")) {
                     Location station_location = new Location(station.getDouble("latitude"), station.getDouble("longitude"));
-                    ford_list.add(new Ford(station_location, Service.start, Service.end));
-                    //also need to set the bike station destinations somehow???
+                    if (Location.displacement_between(station_location, Service.start) < radius) {
+                        ford_list.add(new Ford(station_location, Service.start, Service.end));
+                        //also need to set the bike station destinations somehow???
+                    }
                 }
             }
             return ford_list; //check if this works please
